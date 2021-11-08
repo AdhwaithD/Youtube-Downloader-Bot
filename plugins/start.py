5785495afadd0b89ca76548f43066139273dfb31
Lucifer <93435393+AdhwaithD@users.noreply.github.com>
2021-11-08 15:50:28 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -7,7 +7,7 @@ async def start(client, message):
     joinButton = InlineKeyboardMarkup([
         [InlineKeyboardButton("♻️ Updates ♻️", url="https://t.me/AD_BOTZ12")],
         [InlineKeyboardButton(
-            "🌟 Report Bugs 🌟", url="https://t.me/aryanvikash")]
+            "🌟 Report Bugs 🌟", url="https://t.me/Lucifer_Devil_AD")]
     ])
     welcomed = f"**🌹 ഹായ് 🌹 <b>{message.from_user.first_name}</b>\n/help കൂടുതൽ വിവരങ്ങൾക്ക്**https://telegra.ph/file/ec3d6c6edd45e02c3c125.jpg\n\n"
     await message.reply_text(welcomed, reply_markup=joinButton)
